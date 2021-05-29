56f69bfdb186b161befb07045f1f4b7b6562518d
André <andre.gmaps@gmail.com>
2021-05-28 23:35:15 -0300

    alterando método asJsonString

--- src/test/java/one/digitalinnovation/beerstock/util/JsonUtils.java
+++ src/test/java/one/digitalinnovation/beerstock/util/JsonUtils.java
@@ -3,15 +3,13 @@ package one.digitalinnovation.beerstock.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import one.digitalinnovation.beerstock.dto.BeerDTO;
-
 public class JsonUtils {
 
-	public static String asJsonString(BeerDTO beerDTO) {
+	public static String asJsonString(Object dto) {
 		ObjectMapper objectMapper = new ObjectMapper();
 
 		try {
-			return objectMapper.writeValueAsString(beerDTO);
+			return objectMapper.writeValueAsString(dto);
 		} catch (JsonProcessingException e) {
 			throw new RuntimeException(e);
 		}
